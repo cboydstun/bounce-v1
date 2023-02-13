5b2d88b356c2c866de683c7a81f7369ae2bf3599
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-02-12 20:18:24 -0600

    cleaned up files, but routing still busted

---
+++ pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Hero from '../components/Hero'
+import LeadForm from '../components/LeadForm'
+import IconSection from '../components/IconSection'
+
+export default function Home(props) {
+    return (
+        <div className='home-page-component'>
+            <div className="home-page-container">
+                <Hero props={props} />
+
+                <div className="about-container">
+                    <section className="about-section" id="about-section">
+                        <IconSection />
+                        <LeadForm />
+                    </section>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index/index.page.jsx
+++ pages/index/index.page.jsx
@@ -2,25 +2,13 @@ export { Page }
 
 import React from 'react'
 
-import Hero from '../../components/Hero'
-import LeadForm from '../../components/LeadForm'
-import IconSection from '../../components/IconSection'
+import Home from '../Home'
 
 function Page(props) {
 
   return (
-
-    <div className='home-page-component'>
-      <div className="home-page-container">
-        <Hero props={props} />
-
-        <div className="about-container">
-          <section className="about-section" id="about-section">
-            <IconSection />
-            <LeadForm />
-          </section>
-        </div>
-      </div>
+    <div>
+      <Home props={props} />
     </div>
   )
 }
