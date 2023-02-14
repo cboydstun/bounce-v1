8fdd5cd4ef6c43f99756ecdfa6503cb646e40130
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-02-14 11:00:09 -0600

    add vercel analytics

--- renderer/_default.page.client.jsx
+++ renderer/_default.page.client.jsx
@@ -4,12 +4,15 @@ import React from 'react'
 import { hydrateRoot } from 'react-dom/client'
 import { PageLayout } from './PageLayout'
 
+import { Analytics } from '@vercel/analytics/react';
+
 async function render(pageContext) {
   const { Page } = pageContext
   hydrateRoot(
     document.getElementById('page-view'),
       <PageLayout>
         <Page props={pageContext} />
+        <Analytics />
       </PageLayout>
   )
 }
