eb3ae53c698e45cd1bcbead68122b56046cfbb5b
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-04-04 20:42:59 -0500

    cleanup for redeploy

--- pages/index/index.page.jsx
+++ pages/index/index.page.jsx
@@ -7,21 +7,16 @@ import IconSection from '../../components/IconSection'
 import PaypalForm from '../../components/PayPalForm'
 
 function Page(props) {
-
   return (
-
     <div className='home-page-component'>
       <div className="home-page-container">
         <Hero props={props} />
-
         <div className="about-container">
           <section className="about-section" id="about-section">
-
             <IconSection />
             {/* <LeadForm /> */}
             <PaypalForm />
           </section>
-
         </div>
       </div>
     </div>
