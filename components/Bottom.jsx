35bb0ac4ef42fb6c88cebe126ce455018809bc94
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-04-19 13:02:35 -0500

    more efficient icons and aria labels

--- components/Bottom.jsx
+++ components/Bottom.jsx
@@ -7,13 +7,13 @@ export default function Bottom() {
         <div className="bottom-container">
           <h2>See you at the party! 🥳</h2>
           <p>© 2023 <a href="mailto:satxbounce@gmail.com">SATX Bounce</a>
-            <a href="https://www.facebook.com/profile.php?id=100091608496923" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '10px' }}>
+            <a href="https://www.facebook.com/profile.php?id=100091608496923" target="_blank" rel="noopener noreferrer" aria-label='SATX Bounce on Facebook Social Media App' style={{ marginLeft: '10px' }}>
               <i className="fab fa-facebook-f"></i>
             </a>
-            <a href="https://twitter.com/satxbounce" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '10px' }}>
+            <a href="https://twitter.com/satxbounce" target="_blank" rel="noopener noreferrer" aria-label='SATX Bounce on Twitter Social Media App' style={{ marginLeft: '10px' }}>
               <i className="fab fa-twitter"></i>
             </a>
-            <a href="https://nextdoor.com/pages/satx-bounce-san-antonio-tx/" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '10px' }}>
+            <a href="https://nextdoor.com/pages/satx-bounce-san-antonio-tx/" target="_blank" rel="noopener noreferrer" aria-label='SATX Bounce on Nextdoor Social Media App' style={{ marginLeft: '10px' }}>
               <i className="fa-solid fa-house-chimney fa-flip-horizontal"></i>
             </a>
           </p>
