f9a1511183295afefa29ff3504bb19da12d84eae
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-04-19 13:13:30 -0500

    animated icons

--- components/IconSection.jsx
+++ components/IconSection.jsx
@@ -9,13 +9,13 @@ export default function IconSection() {
                 <h2>Why Choose Us?</h2>
                 <div className="icon-container">
                     <div className='icon-section__icon'>
-                        <i className="fas fa-truck"></i>
+                        <i className="fas fa-truck truck-moves"></i>
                         <h3>Professional and Timely Bounce House Rentals</h3>
                         <p>At SATX Bounce House Rentals, we believe that the customer always comes first. Our goal is to provide exceptional bounce house and inflatable rentals in San Antonio. We understand that your time is valuable, which is why we make it a point to arrive on time for every delivery. Our team of professionals will work tirelessly to ensure that your rental is set up quickly and efficiently, leaving you with plenty of time to enjoy your event. From the moment you book with us, we are committed to providing you with the highest level of customer service.</p>
                     </div>
 
                     <div className='icon-section__icon'>
-                        <i className="fas fa-shield-alt"></i>
+                        <i className="fas fa-shield-alt grow-shrink"></i>
                         <h3>Safe & Clean Inflatable Rentals</h3>
                         <p>We understand that planning a party can be stressful here at SATX Bounce House Rentals. There are so many details to consider, and the last thing you want to worry about is the safety of your guests. That's why we take safety seriously when it comes to our inflatable and party rentals in San Antonio. We only use high-quality, well-maintained equipment, and we follow strict safety protocols to ensure that your event is as safe as possible. You can rest easy knowing that we've done everything in our power to make sure that your party is a success.</p>
                     </div>
@@ -26,13 +26,13 @@ export default function IconSection() {
                     </div>
 
                     <div className='icon-section__icon'>
-                        <i className="fas fa-dollar-sign"></i>
+                        <i className="fas fa-dollar-sign rotates"></i>
                         <h3>Affordable Daily Pricing</h3>
                         <p>At SATX Bounce House Rentals, we believe in transparency when it comes to pricing our bounce house rentals and chair rentals near you. That's why we make it clear that all of our prices are reflected in daily amounts. If you need your rental for more than one day, simply let us know, and we will work with you to find a solution that fits your budget. We want you to feel comfortable with your rental agreement and understand exactly what you're paying for. If you have any questions about pricing, please don't hesitate to ask.</p>
                     </div>
 
                     <div className='icon-section__icon'>
-                        <i className="fas fa-gift"></i>
+                        <i className="fas fa-gift bounce-wiggles"></i>
                         <h3>Free Delivery & Setup for Party Rentals</h3>
                         <p>We believe in making things as easy as possible for our customers in San Antonio. That's why we include delivery and setup in the price of your party rentals. We will work with you to schedule a delivery time that works for you, and our team will arrive promptly before 8 am to set up your rental. When your event is over, we will return to pick up the rental after 6 pm. You won't have to worry about a thing; we've got you covered from start to finish.</p>
                     </div>
