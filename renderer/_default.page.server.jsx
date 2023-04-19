4793aa8f50848131b5d8bbdd961456f3feb631d1
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-04-19 09:35:15 -0500

    added favicon

--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -43,6 +43,10 @@ async function render(pageContext) {
         <meta name="twitter:description" content="Get high-quality and affordable bounce house, inflatable, and party rentals in San Antonio with SATX Bounce House Rentals and More. Book now!">
         <meta name="twitter:image" content="https://www.funasfam.com/wp-content/uploads/2023/04/OG-image-min-v2-min.jpg">
         <meta name="twitter:image:alt" content="SATX Bounce House Rentals and More">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
         ${headWithGtagHtml}
         <title>SATX Bounce House & Inflatable Rentals - San Antonio, TX</title>
       </head>
