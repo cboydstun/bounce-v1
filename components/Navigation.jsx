725a760c28b0354e74292e023addafda9811c71d
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-05-01 01:46:05 -0500

    add mobile hamburger nav

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import './Navigation.css'
 
 export default function Navigation(props) {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    const handleMobileMenuToggle = () => {
+        setMobileMenuOpen(!mobileMenuOpen)
+    }
+
     return (
         <div className='navigation-component'>
             <div className='navigation-container'>
+                <div className='logo-container'>
+                    <a href='/'>
+                        <span role="img" aria-label="Party Popper Emoji" className='logo'>&#x1F389;</span> SATX Bounce
+                    </a>
+                </div>
+
                 <nav>
-                    <ul>
+                    <ul className={`nav-menu ${mobileMenuOpen ? 'nav-menu-mobile' : ''}`}>
                         <li>
                             <a className="navitem" href="/">
                                 Home
@@ -22,6 +36,9 @@ export default function Navigation(props) {
                             </a>
                         </li>
                     </ul>
+                    <div className='nav-icon' onClick={handleMobileMenuToggle}>
+                        {mobileMenuOpen ? <FaTimes /> : <FaBars />}
+                    </div>
                 </nav>
             </div>
         </div>
