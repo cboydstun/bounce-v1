18cd7770998400209915a74ac1e23c6f8723a75b
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-05-29 10:04:48 -0500

    remove popup

--- renderer/PageLayout.jsx
+++ renderer/PageLayout.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 import './PageLayout.css'
 import Navigation from '../components/Navigation'
 import Bottom from '../components/Bottom'
-import ZipCodeModal from '../components/ZipCodeModal'
+// import ZipCodeModal from '../components/ZipCodeModal'
 
 function PageLayout({ children }) {
   return (
     <React.StrictMode>
       <div className="App">
-        <ZipCodeModal />
+        {/* <ZipCodeModal /> */}
         <Navigation name="Bounce SATX" />
         {children}
         <Bottom />
