96449aac5825f86b3b4a23a8fb86479cd16af83a
Chris Boydstun <chrisboydstun@gmail.com>
2023-09-04 12:26:09 -0500

    remove scrolling deal

--- renderer/PageLayout.jsx
+++ renderer/PageLayout.jsx
@@ -12,7 +12,6 @@
 import React from 'react'
 
 import './PageLayout.css'
-import TopBanner from '../components/TopBanner'
 import Navigation from '../components/Navigation'
 import Bottom from '../components/Bottom'
 
@@ -20,7 +19,6 @@ function PageLayout({ children }) {
   return (
     <React.StrictMode>
       <div className="App">
-        <TopBanner text="Hurry! Get 25% off our entire inventory for LABOR DAY - September 4th, 2023! Only while supplies last!" />
         <Navigation />
         {children}
         <Bottom />
