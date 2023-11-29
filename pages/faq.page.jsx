6b0853cca6620d7d1e9e93a4e851a707ea29b6d0
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-11-29 05:51:59 -0600

    faq page keyword update

--- pages/faq.page.jsx
+++ pages/faq.page.jsx
@@ -9,11 +9,15 @@ function Page() {
 
   const faqs = [
     {
-      question: 'When do you deliver the bounce houses?',
-      answer: 'We deliver the bounce houses between 8 am and 10 am, right to your door!'
+      question: 'Where do you offer bounce house rentals?',
+      answer: 'We offer bounce rentals in San Antonio, TX. We also offer bounce rentals in other parts of Bexar County. Please contact us for more information.'
     },
     {
-      question: 'What payment methods do you accept?',
+      question: 'When do you deliver the bounce houses and waterslide rentals?',
+      answer: 'We like deliver the bounce houses early between 8 am and 10 am, right to your door! However, we often have open availability and are happy to work out a time that is best for you.'
+    },
+    {
+      question: 'What payment methods do you accept for bounce houses and water slide rentals?',
       answer: 'We accept cash, CashApp, and credit card payments through PayPal.'
     },
     {
@@ -21,19 +25,19 @@ function Page() {
       answer: 'No, you don\'t need a generator unless your party location is far from an electrical outlet.'
     },
     {
-      question: 'Can I keep the bounce houses overnight?',
+      question: 'Can I make a bounce house rental overnight?',
       answer: 'Yes, we offer overnight rentals for an additional fee. Please let us know in advance if you\'d like to keep them longer.'
     },
     {
-      question: 'Are the bounce houses safe?',
+      question: 'Is a bounce house safe?',
       answer: 'Absolutely! Safety is our top priority. However, we do ask that you keep an eye on the bouncers at all times to ensure everyone\'s safety.'
     },
     {
-      question: 'Can we wear shoes inside the bounce houses?',
+      question: 'Can we wear shoes inside SATX bounce houses?',
       answer: 'For safety reasons, we kindly request that you jump in with bare or socked feet only. No shoes allowed!'
     },
     {
-      question: 'How long can we rent the bounce houses for?',
+      question: 'How long can we rent the bounce houses or jumping place for?',
       answer: 'Our rentals are for the entire day! You can enjoy the bounce houses from the time of delivery until pickup.'
     },
     {
@@ -53,19 +57,19 @@ function Page() {
       answer: 'Yes, we offer discounts for multiple rentals. Please contact us for more information.'
     },
     {
-      question: 'Do you offer discounts for non-profit organizations?',
+      question: 'Do you offer discounts for non-profit organizations for bounce house and water slide rentals?',
       answer: 'Yes, we offer discounts for non-profit organizations. Please contact us for more information.'
     },
     {
-      question: 'Do you offer discounts for military personnel?',
+      question: 'Do you offer discounts on inflatable castles and waterslides for military personnel?',
       answer: 'Yes, we offer discounts for military personnel. Please contact us for more information.'
     },
     {
-      question: 'Do you offer discounts for first responders?',
+      question: 'Do you offer discounts for first responders in San Antonio on bounce houses and water slides?',
       answer: 'Yes, we offer discounts for first responders. Please contact us for more information.'
     },
     {
-      question: 'Do you offer discounts for teachers?',
+      question: 'What about discounts for teachers and other community organizations in San Antonio?',
       answer: 'Yes, we offer discounts for teachers. Please contact us for more information.'
     },
     {
@@ -73,12 +77,12 @@ function Page() {
       answer: 'Yes, we offer discounts for healthcare workers. Please contact us for more information.'
     },
     {
-      question: 'How do I book a bounce house?',
+      question: 'How do I book San Antonio bounce house rentals?',
       answer: 'You can book a bounce house by calling us at (512) 210-0194 or by filling out the contact form on our website.'
     },
     {
-      question: 'How far in advance should I book a bounce house?',
-      answer: 'We recommend booking at least 2 weeks in advance to ensure availability. However, we can sometimes accommodate last-minute requests. Please contact us for more information.'
+      question: 'How far in advance should I book a bounce house near me?',
+      answer: 'We recommend booking at least a week in advance to ensure availability. However, we can sometimes accommodate last-minute requests. Please contact us for more information.'
     },
     {
       question: 'Do you offer gift certificates?',
@@ -86,18 +90,18 @@ function Page() {
     },
     {
       question: 'Are there deals on party packages?',
-      answer: 'Yes, we offer party packages. Please contact us for more information.'
+      answer: 'Yes, we offer party packages and discounts on inflatable bounce houses and waterslides. Please contact us for more information.'
     },
     {
       question: 'What about party supplies?',
       answer: 'Yes, we have cotton candy machines, popcorn machines, snow cone maker and more. Please contact us for more information.'
     },
     {
-      question: 'Are tables and chairs available for rental?',
+      question: 'Are tables and chairs available with bounce house rentals in San Antonio, TX?',
       answer: 'Yes, we offer tables and chairs. Each folding table comes with six folding chairs. Please contact us for more information.'
     },
     {
-      question: 'Do you offer pony rides or petting zoos?',
+      question: 'Do you offer pony rides or petting zoos with bounce house rental in San Antonio, TX?',
       answer: 'Yes, we are partnered with GIDDY UP PONY PARTIES AND PETTING ZOOS, LLC to offer pony rides and petting zoo experiences!'
     }
   ];
