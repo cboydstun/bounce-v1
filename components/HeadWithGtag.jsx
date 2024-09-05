0567e863515b90ed62e893b93de229241718d681
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-09-05 14:41:33 -0500

    added google tag manager

--- components/HeadWithGtag.jsx
+++ components/HeadWithGtag.jsx
@@ -8,8 +8,20 @@ const HeadWithGtag = () => {
     gtag('config', '${import.meta.env.VITE_GOOGLE_ANALYTICS_ID}');
   `;
 
+  const gtmScript = `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${import.meta.env.VITE_GTM_ID}');
+  `;
+
   return (
     <>
+      {/* Google Tag Manager */}
+      <script dangerouslySetInnerHTML={{ __html: gtmScript }}></script>
+
+      {/* Google Analytics */}
       <script async src={`https://www.googletagmanager.com/gtag/js?id=${import.meta.env.VITE_GOOGLE_ANALYTICS_ID}`}></script>
       <script dangerouslySetInnerHTML={{ __html: gtagScript }}></script>
     </>
--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -219,6 +219,10 @@ async function render(pageContext) {
           <title>${pageTitle}</title>
         </head>
         <body>
+          <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${import.meta.env.VITE_GTM_ID}"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <!-- End Google Tag Manager (noscript) -->
           <div id="page-view">${dangerouslySkipEscape(viewHtml)}</div>
         </body>
       </html>`;
